172df72930a89cbd7f9b1b5cd8989795acbf5357
alextricity25 <miguel.cantu@rackspace.com>
2017-01-09 11:14:33 -0600

    Fixed glimpse ipv4 address lookup

--- cloud_providers/rackspace/glimpse.py
+++ cloud_providers/rackspace/glimpse.py
@@ -18,8 +18,28 @@ class Glimpse():
         for server in self.client.servers.list():
             if re.match(self.regex_pattern, server.name):
                 self.pt.add_row([server.name,
-                                 server.addresses['public'][1]['addr']])
+                                 self._get_ipv4_address(server.addresses)])
                 server_list.append(server.name)
 
         print self.pt
+
+    def _get_ipv4_address(self, addresses):
+        """
+        Takes in a dictionary of addresses of the form:
+        {u'private': [{u'addr': u'10.208.128.99', u'version': 4}],
+         u'public': [{u'addr': u'2001:4802:7802:101:be76:4eff:fe20:e736',
+                      u'version': 6},
+                     {u'addr': u'162.242.251.65', u'version': 4}]}
+        :return: string representing an ipv4 address
+        """
+        for key, value in addresses.items():
+            if key == 'public':
+                for addr_info in value:
+                    if "." in addr_info.get("addr", ""):
+                        return addr_info['addr']
+
+        return "Address not found"
+
+
+
 
